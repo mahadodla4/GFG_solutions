75e38887c7e2dacd75451ef0e9ccbbbaf4d5d195
maha <97425755+maha004dodla@users.noreply.github.com>
2023-09-22 19:08:16 +0530

    Updated Solution - GfG to GitHub

---
+++ Medium/First and last occurrences of x/first-and-last-occurrences-of-x.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    vector<int> find(int arr[], int n , int x )
+    {
+        int a=lower_bound(arr,arr+n,x)-arr;
+        int b=upper_bound(arr,arr+n,x)-arr;
+        if(arr[b-1]!=x)
+        {
+            return {-1,-1};
+        }
+        return {a,b-1};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        int arr[n],i;
+        for(i=0;i<n;i++)
+        cin>>arr[i];
+        vector<int> ans;
+        Solution ob;
+        ans=ob.find(arr,n,x);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
