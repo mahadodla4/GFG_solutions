2ec960a22b409bb28ecf572b94d57e544d84d8cb
maha <97425755+maha004dodla@users.noreply.github.com>
2024-01-10 18:04:11 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Modular Exponentiation for large numbers/modular-exponentiation-for-large-numbers.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+		long long int PowMod(long long int x,long long int n,long long int M)
+		{
+		    long long int res=1;
+		    while(n)
+		    {
+		        if(n&1)
+		        {
+		            n=n-1;
+		            res=((res*x)%M);
+		        }
+		        else
+		        {
+		            n=n/2;
+		            x=((x*x)%M);
+		        }
+		    }
+		    return res;
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	long long int x, n, m;
+    	cin >> x >> n >> m;
+    	Solution ob;
+    	long long int ans = ob.PowMod(x, n, m);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
