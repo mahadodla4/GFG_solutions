b59baadb4a9d04ed958528c398bce83ef91ff668
Mahalakshmi Dodla <97425755+mahadodla4@users.noreply.github.com>
2024-10-18 21:09:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Single Number/single-number.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int getSingle(vector<int>& arr) {
+        unordered_map<int,int>mp;
+        for(auto i:arr) {
+            mp[i]++;
+        }
+        for(auto i:mp) {
+            if(i.second%2!=0) {
+                return i.first;
+            }
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.getSingle(arr);
+        cout << res << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
